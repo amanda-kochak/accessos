78e46ac99b6ae68a54f7de81c5ec4aa15e58f38b
Amanda Kochak <amanda.kochak@ischool.berkeley.edu>
2022-05-06 01:35:34 -0700

    commit-5.6

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -45929,7 +45929,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63809" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64642" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
